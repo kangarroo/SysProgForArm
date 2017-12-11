02df6d928545c683d58a693b1a2cb7693f820349
Koen Arroo <kma517@its.york.ac.uk>
2017-12-11 17:07:14 +0000

    Functioning Scheduler, fixed bugs

--- DocetOS_base/fixedPriorityScheduler.c
+++ DocetOS_base/fixedPriorityScheduler.c
@@ -15,6 +15,7 @@ typedef struct {
 static void fixedPriority_addTask(OS_TCB_t * const tcb);
 static void fixedPriority_taskExit(OS_TCB_t * const tcb);
 static OS_TCB_t const * fixedPriority_scheduler(void);
+static void preemptive_tasks (OS_TCB_t * const tcb);
 
 
 priority_list_t priority[MAX_PRIORITY] = {0};
@@ -49,45 +50,78 @@ static void fixedPriority_addTask(OS_TCB_t * const tcb){
 }
 
 /*Task Exit Callback*/
-/**/
+/*This function removes a task from the scheduler this is achieved through
+if the list only has 1 item setting the head and tail of the list to 0, and otherwise
+setting the previous element in the list as the tail*/
 static void fixedPriority_taskExit(OS_TCB_t * const tcb){
 	priority_list_t *current_list = &priority[tcb->priority];
+	OS_TCB_t *current_task = current_list->tail;
+	
 	if(current_list->head == current_list->tail){
-		current_list->head = 0;
-		current_list->tail = 0;
+		//If the list only has one object, the head and tail pointers should be set
+		//to 0, it should be noted that this doesn't free the memory used by the system...
+		if(current_list->tail == tcb){
+			current_list->head = 0; //
+			current_list->tail = 0;
+		}
 	} else {
-		OS_TCB_t *new_tail = current_list->tail->prev;
-		new_tail->next = 0;
-		current_list->tail = new_tail; 
+			while(current_task != 0){
+				if(current_task == tcb){
+					if(current_task == current_list->tail){
+						//If the current element is the tail, the set the previous element as the tail
+						current_list->tail = current_list->tail->prev;
+						current_list->tail->next = 0;
+					} if(current_task->prev == 0){
+						//Remove TCB from head of list
+						current_list->head = current_list->head->next;
+						current_list->head->prev = 0;
+					} else {
+						//If the current element is in the middle of the list, the next and previous
+						//of the surrounding elements need to be updated
+
+						current_task->prev->next = current_task->next;
+						current_task->next->prev = current_task->prev;
+					}
+					return;
+				} else { //Iterates through the list to find the element to be removed
+					current_task = current_task->prev;
+				}
+			}
 	}
 }
 
 
 
 /*Fixed Priority Scheduler*/
-
+/*This function works by iterating through the prioirty array*/
 static OS_TCB_t const * fixedPriority_scheduler(void){
-	OS_currentTCB()->state &= ~TASK_STATE_YIELD;
+	OS_currentTCB()->state &= ~TASK_STATE_YIELD; //Clear the task yield state
 	for(int i = 0;i < (MAX_PRIORITY);i++){
 		priority_list_t *current_list = &priority[i];
 		if(current_list->tail != 0){
-			OS_TCB_t *next_task = current_list->tail; 
-			OS_TCB_t *test = current_list->tail; 
-			fixedPriority_taskExit(next_task);
-			fixedPriority_addTask(next_task);
+			OS_TCB_t *next_task = current_list->tail;
+			//preemptive_tasks(next_task);
+				fixedPriority_taskExit(next_task);
+				fixedPriority_addTask(next_task);
 			//Checks if sleep bit is set
 			if(current_list->tail->state & TASK_STATE_SLEEP){
 				if(current_list->tail->sleep_time <= OS_elapsedTicks()){
 					//If set & time exceeded, clear sleep bit and return TCB, otherwise
 					//return idle tcb
 					current_list->tail->state &= ~TASK_STATE_SLEEP;
-					return current_list->tail;
+					return next_task;
 				}
 			} else {
-				return current_list->tail;
+				return next_task;
 			}
 		}
 	}
 	return OS_idleTCB_p;
 }
+
+static void preemptive_tasks (OS_TCB_t * const tcb){
+	fixedPriority_taskExit(tcb);
+	fixedPriority_addTask(tcb);
+}
+
 
--- DocetOS_base/main.c
+++ DocetOS_base/main.c
@@ -11,7 +11,7 @@ void task1(void const *const args) {
 }
 
 void task2(void const *const args) {
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		printf("B");
 	}
 }
@@ -23,7 +23,7 @@ void task3(void const *const args) {
 }
 
 void task4(void const *const args) {
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		printf("D");
 	}
 }
@@ -35,7 +35,7 @@ void task5(void const *const args) {
 }
 
 void task6(void const *const args) {
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		printf("F");
 	}
 }
@@ -55,12 +55,12 @@ int main(void) {
 	static OS_TCB_t TCB1, TCB2, TCB3, TCB4, TCB5, TCB6;
 
 	/* Initialise the TCBs using the two functions above */
-	OS_initialiseTCB(&TCB1, stack1+64, task1,NULL, 1);//A
-	OS_initialiseTCB(&TCB2, stack2+64, task2,NULL, 0);//B
-	OS_initialiseTCB(&TCB3, stack3+64, task3,NULL, 1);//C
-	OS_initialiseTCB(&TCB4, stack4+64, task4,NULL, 2);//D
-	OS_initialiseTCB(&TCB5, stack5+64, task5,NULL, 0);//E
-	OS_initialiseTCB(&TCB6, stack6+64, task6,NULL, 1);//F
+	OS_initialiseTCB(&TCB1, stack1+64, task1,NULL, 0);//A
+	OS_initialiseTCB(&TCB2, stack2+64, task2,NULL, 1);//B
+	OS_initialiseTCB(&TCB3, stack3+64, task3,NULL, 2);//C
+	OS_initialiseTCB(&TCB4, stack4+64, task4,NULL, 0);//D
+	OS_initialiseTCB(&TCB5, stack5+64, task5,NULL, 1);//E
+	OS_initialiseTCB(&TCB6, stack6+64, task6,NULL, 2);//F
 
 
 	/* Initialise and start the OS */
